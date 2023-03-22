f1dcb9e112d7d428dc3ba4f44c559f021dbf5d50
Setor Kudiabor <kek265@cornell.edu>
2023-03-21 22:36:29 -0400

    Created YAML file for MS2 submission
    - Added file to Final-Project
    - netid:

--- arcade/bin/main.ml
+++ arcade/bin/main.ml
@@ -4,10 +4,6 @@ let data_dir_prefix = "data" ^ Filename.dir_sep
 
 (** [main ()] prompts for the game to play, then starts it. *)
 let main () =
-  ANSITerminal.print_string [ ANSITerminal.red ]
-    "\n\nWelcome to the 3110 Text Adventure Game engine.\n";
-  print_endline "Please enter the name of the game file you want to load.\n";
-  print_endline Checkers.default_fen;
   print_string "> ";
   match read_line () with
   | exception End_of_file -> ()
