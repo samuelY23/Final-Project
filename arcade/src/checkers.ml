5360880f9cc3d063baef507f448528ea978b4aa4
Adam Issah <abi6@cornell.edu>
2023-03-21 21:30:32 -0400

    Made changes to interface in Checkers

---
+++ arcade/src/checkers.ml
@@ -0,0 +1,63 @@
+type player =
+  | Player_1 of int
+  | Player_2 of int
+
+type board = {
+  state : string;
+  turn : player;
+  players : player list;
+}
+
+type piece =
+  | White of char
+  | Black of char
+
+let default_fen = {|X1X1X1X1/1X1X1X1X/X1X1X1X1/8/8/1o1o1o1o/o1o1o1o1/1o1o1o1o|}
+
+let is_digit = function
+  | '1' .. '9' -> true
+  | _ -> false
+
+let string_to_list s = List.init (String.length s) (String.get s)
+
+let rec repeat s n =
+  match n with
+  | 0 -> ""
+  | m -> s ^ repeat s (m - 1)
+
+let rec row_builder lst_f =
+  match lst_f with
+  | [] -> "|"
+  | s :: t ->
+      let raw_char = String.make 1 s in
+      (if is_digit s then repeat "|   " (int_of_string raw_char)
+      else "| " ^ raw_char ^ " ")
+      ^ row_builder t
+
+let rec make_board n fen =
+  let fen_state = String.split_on_char '/' fen in
+  match n with
+  | 0 ->
+      print_endline
+        "  +---+---+---+---+---+---+---+---+\n    a   b   c   d   e   f   g   h"
+  | f ->
+      print_endline
+        ("  +---+---+---+---+---+---+---+---+\n" ^ string_of_int n ^ " "
+        ^ row_builder (string_to_list (List.nth fen_state (8 - n))));
+      make_board (n - 1) fen
+
+(*------------------------------------*)
+(*State*)
+let board_init =
+  {
+    state = default_fen;
+    turn = Player_1 0;
+    players = [ Player_1 0; Player_2 0 ];
+  }
+
+let default_fen = raise (Failure "Unimplemented")
+let current_state = raise (Failure "Unimplemented")
+let current_turn = raise (Failure "Unimplemented")
+let p1_score = raise (Failure "Unimplemented")
+let p2_score = raise (Failure "Unimplemented")
+let update_boardstate = raise (Failure "Unimplemented")
---
+++ arcade/src/checkers.mli
@@ -0,0 +1,18 @@
+type player
+(** The abstract type of player. *)
+
+type board
+(** The abstract type of checkers board. *)
+
+type piece
+(** The abstract type of checker pieces. *)
+
+val default_fen : string
+val current_state : string
+val current_turn : player
+val p1_score : int
+val p2_score : int
+val is_digit : char -> bool
+val string_to_list : string -> char list
+val board_init : board
+val update_boardstate : string -> string
